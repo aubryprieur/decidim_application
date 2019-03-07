ebad2e188433e57d45770e383d8dcf47d65ef3aa
aubry prieur <aubry.prieur@gmail.com>
2019-03-07 11:31:32 +0100

    config storage and email

--- Gemfile
+++ Gemfile
@@ -14,7 +14,7 @@ gem "puma", "~> 3.0"
 gem "uglifier", "~> 4.1"
 
 gem "faker", "~> 1.9"
-
+gem "fog-aws"
 
 group :development, :test do
   gem "byebug", "~> 10.0", platform: :mri
--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 Decidim.configure do |config|
-  config.application_name = "My Application Name"
-  config.mailer_sender = "change-me@domain.org"
+  config.application_name = "Plateforme de participation COPAS"
+  config.mailer_sender = "contact@copas.coop"
 
   # Change these lines to set your preferred locales
-  config.default_locale = :en
-  config.available_locales = [:en, :ca, :es]
+  config.default_locale = :fr
+  config.available_locales = [:fr, :en]
 
   # Geocoder configuration
   # config.geocoder = {

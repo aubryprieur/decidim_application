67f68b6fe9754f6f7d3bf87dce5850104ae58086
aubry prieur <aubry.prieur@gmail.com>
2019-08-27 16:24:11 +0200

    debug carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,12 +2,12 @@
 
 # Default CarrierWave setup.
 #
-CarrierWave.configure do |config|
-  config.permissions = 0o666
-  config.directory_permissions = 0o777
-  config.storage = :file
-  config.enable_processing = !Rails.env.test?
-end
+# CarrierWave.configure do |config|
+#   config.permissions = 0o666
+#   config.directory_permissions = 0o777
+#   config.storage = :file
+#   config.enable_processing = !Rails.env.test?
+# end
 
 # Setup CarrierWave to use Amazon S3. Add `gem "fog-aws" to your Gemfile.
 #
@@ -19,7 +19,7 @@ if Rails.env.production?
       aws_access_key_id:     ENV["BUCKETEER_AWS_ACCESS_KEY_ID"],     # required
       aws_secret_access_key: ENV["BUCKETEER_AWS_SECRET_ACCESS_KEY"], # required
       region:                'eu-west-1',                                       # optional, defaults to 'us-east-1'
-      host:                  's3.eu-west-1.amazonaws.com',                                  # optional, defaults to nil
+      # host:                  's3.eu-west-1.amazonaws.com',                                  # optional, defaults to nil
       # endpoint:              'https://s3.example.com:8080'                      # optional, defaults to nil
     }
     config.fog_directory  = ENV["BUCKETEER_BUCKET_NAME"]                                # required
